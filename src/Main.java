febb95f4ed74d1454ae56c88c62e7cf52230492b
Roman <94304258+metakotleta@users.noreply.github.com>
2022-10-26 14:26:28 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -16,10 +16,11 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080);
-        Socket clientSocket = serverSocket.accept(); // ждем подключения
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
+        synchronized(this) {
+            Socket clientSocket = serverSocket.accept(); // ждем подключения
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        }
         System.out.println("New connection accepted");
 
         final String name = in.readLine();
